b1f9620129ca095e1bb39d84b0e915011e43a6ad
Mauricio <mauriciomeza95@hotmail.com>
2022-01-11 11:15:37 -0600

    Suma realizada

--- calculator.js
+++ calculator.js
@@ -1,13 +1,46 @@
+//FUNCIONES DECLARATIVAS
+//SOLO SE HACE UNA VEZ
+//function
 function sum(a, b) {
-  return;
+
+  //EN CASO DE QUE NO HAYA ARGUMENTOS
+  if (!a && !b) {
+    return 0
+  }
+
+  //EN CASO DE QUE EL SEGUNDO ARGUMENTO NO EXISTA 
+  if(!b) {
+    b = 0
+  }
+
+  const total = a + b
+  return total;
 }
+//INVOCACIÓN DE FUNCIÓN 
+//PUEDE USARSE (INVOCARSE) LAS VECES QUE SE QUIERA
+//sum (1,2)
+
 
 function subtract(a, b) {
-  return;
+ if (!a && !b) {
+   return 0
+ }
+ if (!b) {
+   b = 0
+ }
+  const total = a - b
+  return total;
 }
 
 function divide(a, b) {
-  return;
+  if (!a && !b) {
+    return 0
+  }
+  if (!a || !b) {
+    return error
+  }
+  const total = a / b
+  return total;
 }
 
 function multiply(a, b) {
